5577d4fb2dc244de5aed0e480b64eb6e42d40456
Daemon <daemon336699@gmail.com>
2019-03-17 21:57:21 +0800

    move DefaultLogger to separate file

---
+++ logger/src/main/kotlin/me/daemon/logger/DefaultLogger.kt
@@ -0,0 +1,58 @@
+package me.daemon.logger
+
+import android.util.Log
+
+/**
+ * @author daemon
+ * @since 2019-03-17 21:56
+ */
+class DefaultLogger : ILogger {
+
+    private var tag: String = ""
+
+    override fun setTag(tag: Any): ILogger {
+        this.tag = tag.toString()
+        return this
+    }
+
+    override fun v(vararg msg: Any) {
+        if (level.ordinal > Level.VERBOSE.ordinal) {
+            return
+        }
+
+        Log.v(tag, concatMessage(*msg))
+    }
+
+    override fun d(vararg msg: Any) {
+        if (level.ordinal > Level.DEBUG.ordinal) {
+            return
+        }
+
+        Log.d(tag, concatMessage(*msg))
+    }
+
+    override fun i(vararg msg: Any) {
+        if (level.ordinal > Level.INFO.ordinal) {
+            return
+        }
+
+        Log.i(tag, concatMessage(*msg))
+    }
+
+    override fun w(vararg msg: Any) {
+        if (level.ordinal > Level.WARN.ordinal) {
+            return
+        }
+
+        Log.w(tag, concatMessage(*msg))
+    }
+
+    override fun e(vararg msg: Any) {
+        if (level.ordinal > Level.ERROR.ordinal) {
+            return
+        }
+
+        Log.e(tag, concatMessage(*msg))
+    }
+
+}
--- logger/src/main/kotlin/me/daemon/logger/Logger.kt
+++ logger/src/main/kotlin/me/daemon/logger/Logger.kt
@@ -2,8 +2,6 @@
 
 package me.daemon.logger
 
-import android.util.Log
-
 /**
  * @author daemon
  * @since 2019-03-16 21:51
@@ -93,63 +91,12 @@ fun e(tag: String, vararg msg: Any) {
     get().setTag(tag).e(*msg)
 }
 
-private fun concatMessage(vararg msg: Any): String {
+internal fun concatMessage(vararg msg: Any): String {
     val sb = StringBuilder()
     for (m in msg) {
         sb.append(m)
     }
 
     return sb.toString()
-}
-
-class DefaultLogger : ILogger {
-
-    private var tag: String = ""
-
-    override fun setTag(tag: Any): ILogger {
-        this.tag = tag.toString()
-        return this
-    }
-
-    override fun v(vararg msg: Any) {
-        if (level.ordinal > Level.VERBOSE.ordinal) {
-            return
-        }
-
-        Log.v(tag, concatMessage(*msg))
-    }
-
-    override fun d(vararg msg: Any) {
-        if (level.ordinal > Level.DEBUG.ordinal) {
-            return
-        }
-
-        Log.d(tag, concatMessage(*msg))
-    }
-
-    override fun i(vararg msg: Any) {
-        if (level.ordinal > Level.INFO.ordinal) {
-            return
-        }
-
-        Log.i(tag, concatMessage(*msg))
-    }
-
-    override fun w(vararg msg: Any) {
-        if (level.ordinal > Level.WARN.ordinal) {
-            return
-        }
-
-        Log.w(tag, concatMessage(*msg))
-    }
-
-    override fun e(vararg msg: Any) {
-        if (level.ordinal > Level.ERROR.ordinal) {
-            return
-        }
-
-        Log.e(tag, concatMessage(*msg))
-    }
-
 }
 
