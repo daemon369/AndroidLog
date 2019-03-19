df5d1290b6390486e60a944bf40bbd6ef101ec23
Daemon <daemon336699@gmail.com>
2019-03-19 11:45:53 +0800

    add new functions

--- logger/src/main/kotlin/me/daemon/logger/DefaultLogger.kt
+++ logger/src/main/kotlin/me/daemon/logger/DefaultLogger.kt
@@ -16,43 +16,63 @@ open class DefaultLogger : ILogger {
     }
 
     override fun v(vararg msg: Any) {
+        vt(tag, *msg)
+    }
+
+    override fun vt(tag: Any, vararg msg: Any) {
         if (level.ordinal > Level.VERBOSE.ordinal) {
             return
         }
 
-        Log.v(tag, concatMessage(*msg))
+        Log.v(tag.toString(), concatMessage(*msg))
     }
 
     override fun d(vararg msg: Any) {
+        dt(tag, *msg)
+    }
+
+    override fun dt(tag: Any, vararg msg: Any) {
         if (level.ordinal > Level.DEBUG.ordinal) {
             return
         }
 
-        Log.d(tag, concatMessage(*msg))
+        Log.d(tag.toString(), concatMessage(*msg))
     }
 
     override fun i(vararg msg: Any) {
+        it(tag, *msg)
+    }
+
+    override fun it(tag: Any, vararg msg: Any) {
         if (level.ordinal > Level.INFO.ordinal) {
             return
         }
 
-        Log.i(tag, concatMessage(*msg))
+        Log.i(tag.toString(), concatMessage(*msg))
     }
 
     override fun w(vararg msg: Any) {
+        wt(tag, *msg)
+    }
+
+    override fun wt(tag: Any, vararg msg: Any) {
         if (level.ordinal > Level.WARN.ordinal) {
             return
         }
 
-        Log.w(tag, concatMessage(*msg))
+        Log.w(tag.toString(), concatMessage(*msg))
     }
 
     override fun e(vararg msg: Any) {
+        et(tag, *msg)
+    }
+
+    override fun et(tag: Any, vararg msg: Any) {
         if (level.ordinal > Level.ERROR.ordinal) {
             return
         }
 
-        Log.e(tag, concatMessage(*msg))
+        Log.e(tag.toString(), concatMessage(*msg))
     }
 
 }
--- logger/src/main/kotlin/me/daemon/logger/ILogger.kt
+++ logger/src/main/kotlin/me/daemon/logger/ILogger.kt
@@ -10,12 +10,22 @@ interface ILogger {
 
     fun v(vararg msg: Any)
 
+    fun vt(tag: Any, vararg msg: Any)
+
     fun d(vararg msg: Any)
+
+    fun dt(tag: Any, vararg msg: Any)
 
     fun i(vararg msg: Any)
 
+    fun it(tag: Any, vararg msg: Any)
+
     fun w(vararg msg: Any)
 
+    fun wt(tag: Any, vararg msg: Any)
+
     fun e(vararg msg: Any)
+
+    fun et(tag: Any, vararg msg: Any)
 
 }
